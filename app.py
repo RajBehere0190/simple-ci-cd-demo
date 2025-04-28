b84a2e72c79a8509ea343c8f0920b58657722ee8
Raj Behere <117808263+RajBehere0190@users.noreply.github.com>
2025-04-28 15:16:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ def add(a, b):
     return a + b
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 6
 
 if __name__ == "__main__":
     print("Simple Addition Result:", add(2, 5))
